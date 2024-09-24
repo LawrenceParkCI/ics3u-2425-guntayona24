279b3e75f8596e11d7a59287305b275879a627b3
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2024-09-24 10:28:58 -0400

    Prints student info and schedule

---
+++ ICS3U1-1 GuntayonA/src/unit1/About_Me.java
@@ -0,0 +1,26 @@
+package unit1;
+/**
+ * Description: This program prints info about me <br>
+ * Date: September 20 <br>
+ * @author Alexander Guntayon
+ */
+
+
+public class About_Me {
+
+	public static void main(String[] args) {
+		//Prints info about me
+		System.out.println("INFORMATION:");
+		System.out.println("______________________________");
+		System.out.println("Name: Alexander");
+		System.out.println("Grade: 11");
+		System.out.println("Teacher: Ms. Kemp");
+		System.out.println("School: Lawrence Park CI");
+		System.out.println("\"Go Pathers\"");
+		System.out.println("______________________________");
+		System.out.println("Schedule");
+		System.out.printf("Day: %-10sTime: %8s", "Monday", "9:00 am");
+		
+	}
+
+}

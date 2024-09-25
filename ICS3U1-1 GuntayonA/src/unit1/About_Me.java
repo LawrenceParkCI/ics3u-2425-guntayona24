2b32b3aa9a7071314dd83fa7899738379c63b883
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2024-09-25 10:27:07 -0400

    Prints student info and time table in a neat format

--- ICS3U1-1 GuntayonA/src/unit1/About_Me.java
+++ ICS3U1-1 GuntayonA/src/unit1/About_Me.java
@@ -17,20 +17,39 @@ public class About_Me {
 		System.out.println("Teacher: Ms. Kemp");
 		System.out.println("School: Lawrence Park CI");
 		System.out.println("\"Go Pathers\"");
-		System.out.println("______________________________");
-		System.out.println("Schedule Sept 23");
-		System.out.println("__________________________________________________");
-		System.out.printf("%1s %1s %14s", "Day 1:", "Monday", "Time\n");
-		System.out.printf("%16s %19s", "Period 1:", "9:00 - 10:15 am \n");
-		System.out.printf("%16s %19s", "Period 2:", "10:20 - 11:40 am \n");
-		System.out.printf("%15s %19s", "Period 3:", "12:40 - 1:55 pm \n");
-		System.out.printf("%16s %19s", "Period 4:", "2:00 - 3:15 pm \n");
-		
-		
-		System.out.printf("%-1s %-12s %-1s %-15s","Day 2:", "Tuesday", "Time:", "9:00 am \n");
-		//System.out.printf("Day 1: %-17sTime: %8s", "Wednesday", "9:55 am \n");
-		//System.out.printf("Day 2: %-17sTime: %8s", "Thursday", "9:00 am \n");
-		//System.out.printf("Day 1: %-17sTime: %8s", "Friday", "9:00 am \n");
+		System.out.println("____________________________________");
+		System.out.printf("%1s %24s %12s", "","Schedule Sept 23", "| \n");
+		System.out.println("____________________________________|");
+		System.out.printf("%2s %1s %24s", "Day 1:", "Monday", "Time        |\n");
+		System.out.printf("%16s %22s", "Period 1:", "9:00 - 10:15 am | \n");
+		System.out.printf("%16s %22s", "Period 2:", "10:20 - 11:40 am | \n");
+		System.out.printf("%16s %22s", "Period 3:", "12:40 -  1:55 pm | \n");
+		System.out.printf("%16s %22s", "Period 4:", "2:00 -  3:15 pm | \n");
+		System.out.printf("%39s", "| \n");
+		System.out.printf("%2s %1s %23s", "Day 2:", "Tuesday", "Time        |\n");
+		System.out.printf("%16s %22s", "Period 1:", "9:00 - 10:15 am | \n");
+		System.out.printf("%16s %22s", "Period 2:", "10:20 - 11:40 am | \n");
+		System.out.printf("%16s %22s", "Period 3:", "12:40 -  1:55 pm | \n");
+		System.out.printf("%16s %22s", "Period 4:", "2:00 -  3:15 pm | \n");
+		System.out.printf("%39s", "| \n");
+		System.out.printf("%2s %1s %21s", "Day 1:", "Wednesday", "Time        |\n");
+		System.out.printf("%16s %22s", "Period 1:", "9:55 - 11:10 am | \n");
+		System.out.printf("%16s %22s", "Period 2:", "11:15 - 12:10 pm | \n");
+		System.out.printf("%16s %22s", "Period 3:", "12:40 -  1:55 pm | \n");
+		System.out.printf("%16s %22s", "Period 4:", "2:00 -  3:15 pm | \n");
+		System.out.printf("%39s", "| \n");
+		System.out.printf("%2s %1s %22s", "Day 2:", "Thrusday", "Time        |\n");
+		System.out.printf("%16s %22s", "Period 1:", "9:00 - 10:15 am | \n");
+		System.out.printf("%16s %22s", "Period 2:", "10:20 - 11:40 am | \n");
+		System.out.printf("%16s %22s", "Period 3:", "12:40 -  1:55 pm | \n");
+		System.out.printf("%16s %22s", "Period 4:", "2:00 -  3:15 pm | \n");
+		System.out.printf("%39s", "| \n");
+		System.out.printf("%2s %1s %24s", "Day 1:", "Friday", "Time        |\n");
+		System.out.printf("%16s %22s", "Period 1:", "9:00 - 10:15 am | \n");
+		System.out.printf("%16s %22s", "Period 2:", "10:20 - 11:40 am | \n");
+		System.out.printf("%16s %22s", "Period 3:", "12:40 -  1:55 pm | \n");
+		System.out.printf("%16s %22s", "Period 4:", "2:00 -  3:15 pm | \n");
+		System.out.println("_____________________________________");
 	}
 
 }

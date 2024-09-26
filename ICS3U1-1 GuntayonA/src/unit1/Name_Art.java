4731b9669dd6beea2b835c2193f4c9a682a01976
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2024-09-26 11:14:46 -0400

    Prints first and last name

---
+++ ICS3U1-1 GuntayonA/src/unit1/Name_Art.java
@@ -0,0 +1,27 @@
+package unit1;
+/**
+ * Description: This program prints my name <br>
+ * 
+ * Date: Sept, 19, 2024 <br>
+ * @author Alexander Guntayon
+ */
+public class Name_Art {
+
+	public static void main(String[] args) {
+	//First Name
+	System.out.println("    _    _                          _           ");
+	System.out.println("   / \\  | | _____  ____ _ _ __   __| | ___ _ __ ");
+	System.out.println("  / _ \\ | |/ _ \\ \\/ / _` | '_ \\ / _` |/ _ \\ '__|");
+	System.out.println(" / ___ \\| |  __/>  < (_| | | | | (_| |  __/ |  ");
+	System.out.println("/_/   \\_\\_|\\___/_/\\_\\__,_|_| |_|\\__,_|\\___|_| ");
+	System.out.println("\n\n");
+	//Last name
+	System.out.println(" __            ___           __       ");
+	System.out.println("/ _` |  | |\\ |  |   /\\  \\ / /  \\ |\\ | ");
+	System.out.println("\\__> \\__/ | \\|  |  /~~\\  |  \\__/ | \\| ");
+	System.out.println("                                      ");
+
+	}
+		
+
+}

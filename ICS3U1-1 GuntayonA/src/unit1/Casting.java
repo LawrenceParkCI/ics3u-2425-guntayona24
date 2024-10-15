7b0bdb17515001256f1bc2da2484d780c2b5ef08
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2024-10-15 09:44:33 -0400

    Casting 1 complete

--- ICS3U1-1 GuntayonA/src/unit1/Casting.java
+++ ICS3U1-1 GuntayonA/src/unit1/Casting.java
@@ -33,16 +33,20 @@ public class Casting {
 	
 	    //What type of data is printed here? Why?
 	    System.out.println(doubleNum);
-	
+	    //A double is printed because the int was assigned to doubleNum which became an Double
+	    
+	    
 	    //can you explain what is happening in this code?
 	    doubleNum = doubleNum + 2.2;
-	
+	    //Adds 2.2 to doubleNum
+	    	
 	    //explicit casting
 	    intNum = (int) doubleNum;
 	
 	    //What type of data is printed here? Why?
 	    System.out.println(intNum);
-	
+	    //It is an int as it casts a double to an int
+	    
 	    /*
 	    Demonstrate casting between each of the data type. Write down whether it is explicit or implicit
 	      int to double
@@ -52,17 +56,47 @@ public class Casting {
 	
 	    Why do you think these are implicit/explicit? (Hint: Think about the size of each data and the type of data)
 	    */
-	
+	    
+	    // implicit usually just turns something into another type, while explicit does the same but the variable that was converted seems to lose its original type
+	    
+	    int myInt = 10;
+	    double myDouble = 0;
+	    
+	    myInt = (int) myDouble;
+	    
+	    myDouble = (double) myInt;
+	    
+	    int myInt2 = 20;
+	    long mylong = 0;
+	    
+	    
+	    myInt2 = (int) mylong;
+	    
+	    mylong = (long) myInt2;
+	    
+	    
+	   //int myInt = 10;
+	   //double myDouble = 10.0;
 	    System.out.println("Part 2");
 	
 	    //understanding what you can do with casting, can you round the following variable to the nearest 10th?
-	    double myNum;
+	    double myNum = 12.2921;
+	    short myNum2 = (short) Math.rint(myNum);
+	    
+	    
+	    System.out.println(myNum);
+	    
 	    
 	    Scanner in = new Scanner(System.in);
 	    System.out.println("Give me decimal number up to the hundredth");
 	    //code
-	
-	    System.out.print("Rounding down to the nearest tenth, it is: ");
 	    
+	    double theNum = in.nextDouble();
+	    short theFloat = 0;
+	    
+	    
+	    System.out.print("Rounding down to the nearest tenth, it is: ");
+	    theFloat = (short) theNum;
+	    System.out.println(Math.rint(theNum));
 	  }
 }

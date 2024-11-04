b313ce46fed3cdc69bac9dd3ce5705e8a22600f0
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2024-11-04 11:24:36 -0500

    PositiveNegative and OddEven finished

---
+++ ICS3U1-1 GuntayonA/src/unit2/PositiveNegative.java
@@ -0,0 +1,38 @@
+package unit2;
+
+import java.util.Scanner;
+
+/**
+* Description: Determines if a number is positive or negative and divisible by 7
+* Date: Nov 1 2024
+* @author Alexander Guntayon
+*/
+
+public class PositiveNegative {
+	/**
+	* This is the entry point to the program.
+	* @param args unused
+	*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type a int:");
+		int input = sc.nextInt();
+		if (input < 0) {
+			System.out.println("This is negative");
+		}
+		else {
+			System.out.println("This is a positive");
+			
+		int inputPos = Math.abs(input);
+		
+		if (inputPos % 7 == 0) {
+			System.out.println("Divisible by 7");
+		}
+		else {
+			System.out.println("not divisible by 7");
+		}
+		}
+		
+	}
+
+}

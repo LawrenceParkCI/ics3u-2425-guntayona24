d9f6a49c07eca2335883c306016921e8f6fc64ef
342899929 <342899929@C-MXL2393F2H.tdsb.on.ca>
2025-01-09 10:14:40 -0500

    WireCutter in progress

--- ICS3U1-1 GuntayonA/src/finalProject/WireCutter.java
+++ ICS3U1-1 GuntayonA/src/finalProject/WireCutter.java
@@ -3,13 +3,46 @@ package finalProject;
 import hsa_new.Console;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
 
 public class WireCutter {
 
 	public static void main(String[] args) {
+		
+		
+		 
+		 
+		 
+		int badWire = wires((int)(Math.random() * 10) + 1);
+		// System.out.println(badWire);
+
 		Scanner sc = new Scanner(System.in);
-		Console c = new Console(50, 1000);
+
+
+	}
+
+	public static int wires(int randWire) {
+		Console c = new Console(50, 1000); // Wires console
+		Console c2 = new Console(2, 10); // Input/output console
+		
+		// Images
+		 BufferedImage explosionJpg = null;
+		
+		 try {
+			explosionJpg = ImageIO.read(new File ("src/finalProjectPictures/Explosion.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int cut; // Stores the int of what wire was cut
+		int wireCutAmount = 0; // The number of good wires cut
+		
+		// Wire Graphics
 		c.setColor(new Color(0, 0, 255));
 		c.fillRect(450, 100, 1000, 25);
 		c.setColor(new Color(255, 0, 0));
@@ -30,11 +63,25 @@ public class WireCutter {
 		c.fillRect(450, 700, 1000, 25);
 		c.setColor(new Color(150, 150, 150));
 		c.fillRect(450, 775, 1000, 25);
-		c.setColor(new Color(255, 255, 255));
-		int cut;
+		
+		
+		// Numbers Graphics for the wires
+		
+		c.setColor(new Color(0, 0, 0));
+		c.setFont(new Font("MonoSpaced", Font.BOLD, 30));
+		c.drawString("1", 1460, 120);
+		
+		
 		
 		do {
-			cut = c.readInt();
+			c.setColor(new Color(0, 0, 0));
+			
+			c.setFont(new Font("MonoSpaced", Font.BOLD, 40));
+			c.drawString("Your Score: " + score(wireCutAmount), 70, 100);
+			
+			c.setColor(new Color(255, 255, 255));
+			
+			cut = c2.readInt();
 			switch(cut) {
 			case 1:
 				c.fillRect(750, 100, 350, 25);
@@ -67,11 +114,38 @@ public class WireCutter {
 				c.fillRect(750, 775, 350, 25);
 				break;
 			default:
-			System.out.println("np");
+				c2.print("Please try again");
 			} 
-		}while (cut != 11);
+			
+			if (cut != randWire) // Checks if user cuts the correct wire to add points to score
+				wireCutAmount += 1;
+				System.out.println((score(wireCutAmount)));
+				c.setColor(new Color(150, 220, 255));
+				c.fillRect(35, 32, 400, 140);
+				
+				
+			c2.clear(); // Clears the input/output console, not the graphics console
+			
+		} while (cut != randWire); 
+		
+		if (cut == randWire) { // When user cuts the wrong wire
+			c2.print("game over");
+			c.drawImage (explosionJpg, -200, -300, 2500, 1500, null);
+		}	
+		
+		int [] wireList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // TODO
+		int badWire = wireList[randWire];
+		return badWire;
 	}
-	public static int wires(int randWire) {
-		int [] wireList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	public static int restartScreen(boolean winCheck) {
+		System.out.println();
+		return 0;
+	}
+	
+	public static int score(int wireCutAmount) {
+		// MAKE AN ARRAY FOR THIS TOIDO
+		int playerScore = wireCutAmount * wireCutAmount;
+		return playerScore;
 	}
 }
